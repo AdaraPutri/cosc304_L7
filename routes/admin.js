16448494297b5834ab156b16c881d117806c5b20
zkagda <zahrakagda28@gmail.com>
2024-11-25 20:12:18 -0800

    got admin page working

--- routes/admin.js
+++ routes/admin.js
@@ -7,6 +7,7 @@ router.get('/', function(req, res, next) {
 
 	
 	// TODO: Include files auth.jsp and jdbc.jsp
+    auth.checkAuthentication(req,res);
     
 	
     res.setHeader('Content-Type', 'text/html');
@@ -17,16 +18,15 @@ router.get('/', function(req, res, next) {
 
 	    // TODO: Write SQL query that prints out total order amount by day
 
-        query="SELECT o.DateTime, o.totalAmount FROM ordersummary o WHERE orderDate=?"
-        let currentDate= newDate();
-        let result= await pool.request()
-                    .input('currentDate',sql.DateTime,currentDate)
-                    .query(query);
+        query="SELECT YEAR(o.orderDate) as year, MONTH(o.orderDate) as month, DAY(o.orderDate) as day, o.totalAmount FROM ordersummary o ORDER BY o.orderDate"
+        
+        let result= await pool.request().query(query);
 
                     res.write("<table border='1'<tr><th>Order Date</th><th>Total Order Amount</th>></tr>");
                     result.recordset.forEach(order=>{
+                        formattedDate= order.year + "-" + order.month + "-" + order.day
                         res.write("<tr>");
-                        res.write("<td>" + order.DateTime + "</td>");
+                        res.write("<td>" + formattedDate + "</td>");
                         res.write("<td>" + order.totalAmount + "</td>");
                         res.write("<tr>");
                     })
